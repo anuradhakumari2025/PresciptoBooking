f7b645074061d39601c5530142f17e153999406e
anuradhakumari2025 <krianuradha4321@gmail.com>
2025-07-07 19:03:53 +0530

    update  in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{port:5173}
+  server:{port:5173},
+    base: './' // 👈 this helps for correct asset paths in production
 })
