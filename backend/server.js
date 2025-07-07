fa83fc0dce3bcc25e4f0eebc0d477197098bfd3c
anuradhakumari2025 <krianuradha4321@gmail.com>
2025-07-07 19:49:57 +0530

    Login spelling change

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const connectDB = require("./config/mongodb");
-const connectCloudinary = require('./config/cloudinary')
+const connectCloudinary = require("./config/cloudinary");
 const doctorRoutes = require("./routes/doctor.route");
 const adminRoutes = require("./routes/admin.route");
 const userRoutes = require("./routes/user.route");
@@ -13,27 +13,27 @@ const userRoutes = require("./routes/user.route");
 
 connectDB();
 connectCloudinary();
-app.use(cors(
-  {origin:"https://presciptobooking-v1.vercel.app/"|| "http://localhost:5173/"}
-  
-));
+app.use(
+  cors({
+    origin:
+      "https://presciptobooking-v1.vercel.app/" || "http://localhost:5173/",
+  })
+);
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 // app.use(cookieParser());
 
 const port = process.env.PORT || 3000;
 
-
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
-app.use('/api/doctor', doctorRoutes);
-app.use('/api/admin',adminRoutes)
-app.use('/api/user',userRoutes)
-
+app.use("/api/doctor", doctorRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/user", userRoutes);
 
 // module.exports = app;
-app.listen(port ,()=>{
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
